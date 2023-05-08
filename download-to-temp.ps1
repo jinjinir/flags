6080c9cc706ffd819e4ee244a71386efaed4d98b
kali-vmware <kali@kali.cc>
2023-05-08 14:20:47 +0800

    add invoke obfuscation output script

--- download-to-temp.ps1
+++ download-to-temp.ps1
@@ -1,1 +1,2 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
 iwr -Uri https://raw.githubusercontent.com/jinjinir/flags/master/flag-1.txt -OutFile $env:temp\flag-1.txt
