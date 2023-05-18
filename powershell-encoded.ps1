03510d0978b73c4744dd1fc64c455c181021789d
kali-vmware <kali@kali.cc>
2023-05-18 12:14:24 +0800

    push changes

--- powershell-encoded.ps1
+++ powershell-encoded.ps1
@@ -1,13 +1,11 @@
-# Set up parameters
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
+
 $url = "https://raw.githubusercontent.com/jinjinir/flags/master/flag-1.txt"
 $outfile = "$env:TEMP\flag.txt"
 
-# Define encoded command
 $command = "Invoke-WebRequest -Uri $url -OutFile $outfile"
 
-# Encode command
 $bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
 $encodedCommand = [Convert]::ToBase64String($bytes)
 
-# Run encoded command with bypassed execution policy and no profile
 Start-Process powershell -WindowStyle Hidden -ArgumentList "-NoProfile", "-ExecutionPolicy", "Bypass", "-EncodedCommand", $encodedCommand
